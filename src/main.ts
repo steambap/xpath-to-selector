9aaad826b97f533fe9c8f3bd2ee322474625954c
Weilin Shi <934587911@qq.com>
2020-09-29 11:17:15 +0800

    to selector

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import Parser from './parser';
+import Parser from "./parser";
+import toSelector from "./to-selector";
 
 export const parse = (query: string) => new Parser(query).parse();
 
@@ -7,7 +8,25 @@ export const isXPath = (query: string) => {
     parse(query);
 
     return true;
-  } catch(e) {
+  } catch (e) {
     return false;
   }
-}
+};
+
+export const xpath2css = (query: string) => toSelector(parse(query));
+
+export default xpath2css;
+
+export const x = (arg: string[], ...param: any[]) => {
+  if (arg.length <= 1) {
+    return xpath2css(arg[0]);
+  }
+
+  let query = arg[0];
+  for (let i = 1; i < arg.length; i++) {
+    query += String(param[i - 1]);
+    query += arg[i];
+  }
+
+  return xpath2css(query);
+};
